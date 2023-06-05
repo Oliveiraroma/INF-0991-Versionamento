e3592c69a155f1049bbd83c01195f3fd974db269
Matheus Ribeiro <matheusribeiro545@gmail.com>
2023-06-04 23:29:21 -0300

    Fix: nova correção e retirada de comando

--- Trabalho2/Program.cs
+++ Trabalho2/Program.cs
@@ -1,10 +1,9 @@
-﻿using System;
-public class Program
+﻿public class Program
 {
     private static void Main(string[] args)
     {
         Console.WriteLine("Commit V1.0");
-        Console.WriteLine("Commit V2.0");
-        Console.WriteLine("Commit V3.1 - correção do projeto");              
+        // Console.WriteLine("Commit V2.0");
+        Console.WriteLine("Commit V3.2 - correção do projeto");              
     }
 }
