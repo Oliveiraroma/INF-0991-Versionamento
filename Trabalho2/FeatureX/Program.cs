4115117b44e15fc68b4622700e04ac82e472034b
Rodrigo Oliveira <oliveiraroma@gmail.com>
2023-06-04 23:43:24 -0300

    FeatureX V2.2

--- Trabalho2/FeatureX/Program.cs
+++ Trabalho2/FeatureX/Program.cs
@@ -3,7 +3,7 @@ public class Program
     private static void Main(string[] args)
     {
         Console.WriteLine("Commit V1.0");
-        Console.WriteLine("Commit V2.1 FeatureX");
+        Console.WriteLine("Commit V2.2 FeatureX");
         Console.WriteLine("Commit V3.0");                   
         Console.WriteLine("Commit V4.0");
     }
