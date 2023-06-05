66250f4835fd28900aadb60106ece55e62b8e6c6
Matheus Ribeiro <matheusribeiro545@gmail.com>
2023-06-04 23:16:27 -0300

    Fix: adicionar biblioteca System

--- Trabalho2/Program.cs
+++ Trabalho2/Program.cs
@@ -1,9 +1,10 @@
-﻿public class Program
+﻿using System;
+public class Program
 {
     private static void Main(string[] args)
     {
         Console.WriteLine("Commit V1.0");
         Console.WriteLine("Commit V2.0");
-        Console.WriteLine("Commit V3.0");                   
+        Console.WriteLine("Commit V3.1 - correção do projeto");              
     }
 }
